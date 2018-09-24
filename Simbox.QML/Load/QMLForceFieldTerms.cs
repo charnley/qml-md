5e9804be97fc1714cb0f087214b1f5bc6c917070
Mike O'Connor <mikeoconnor0308@gmail.com>
2018-09-24 16:42:04 +0100

    functioning plugin template, with auto install to itch.io step.

--- Simbox.QML/Load/QMLForceFieldTerms.cs
+++ Simbox.QML/Load/QMLForceFieldTerms.cs
@@ -9,23 +9,61 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
+using Nano;
+using Nano.Science.Simulation;
 
 namespace Simbox.QML.Load
 {
-    class QMLForceFieldTerms : IForceFieldTerms
+    [XmlName("QMLForceField")]
+    class QMLForceFieldTerms : IForceFieldTerms, IInstantiatedForceFieldTerms
     {
+        /// <summary>
+        /// Add this forcefield to the instantiated topology. 
+        /// </summary>
+        /// <param name="parentTopology">The topology.</param>
+        /// <param name="residue">The atom group this forcefield is associated with.</param>
+        /// <param name="context">The spawn context this forcefield is associated with.</param>
         public void AddToTopology(InstantiatedTopology parentTopology, IResidue residue, ref SpawnContext context)
         {
+            //TODO generalize this to multiple templates/atom groups.
+            parentTopology.ForceFieldTerms.Add(this);
         }
 
+        /// <summary>
+        /// Loads this forcefield from XML file.
+        /// </summary>
+        /// <param name="context">Load context.</param>
+        /// <param name="node">Node from which to load forcefield.</param>
         public void Load(LoadContext context, XmlNode node)
         {
             //TODO add representations & configurations. 
         }
 
+        /// <summary>
+        /// Saves this forcefield to an XML node.
+        /// </summary>
+        /// <param name="context">Load context.</param>
+        /// <param name="element">Node to save the forcefield to.</param>
+        /// <returns></returns>
         public XmlElement Save(LoadContext context, XmlElement element)
         {
+            //TODO save any necessary information.
             return element;
+        }
+
+        /// <summary>
+        /// Generates the forcefield, given the topology.
+        /// </summary>
+        /// <param name="parentTopology">The topology to spawn the forcefield into.</param>
+        /// <param name="properties">Properties of the system.</param>
+        /// <param name="reporter">Reporter to print output to.</param>
+        /// <returns></returns>
+        public IForceField GenerateForceField(InstantiatedTopology parentTopology, SystemProperties properties, IReporter reporter)
+        {
+            //TODO do any extra stuff thats needed.
+            var forcefield = new QMLForceField();
+            reporter.PrintEmphasized("Successfully initialised QML forcefield.");
+            return forcefield;
         }
     }
 }
--- Simbox.QML/QMLForceField.cs
+++ Simbox.QML/QMLForceField.cs
@@ -9,18 +9,30 @@ using System.Threading.Tasks;
 
 namespace Simbox.QML
 {
+    /// <summary>
+    /// A forcefield interface to the QML package.
+    /// </summary>
+    /// <remarks>
+    /// 
+    /// </remarks>
     public class QMLForceField : IForceField
     {
         public string Name => "QML Force Field";
 
+        public QMLForceField()
+        {
+            
+        }
+        
         public float CalculateForceField(IAtomicSystem system, List<Vector3> forces)
         {
-            throw new NotImplementedException();
+            //TODO implement the python calls here.
+            return 0f;
         }
 
         public void Dispose()
         {
-            throw new NotImplementedException();
+            //Dispose of things here.
         }
     }
 }
