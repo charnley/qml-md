181d31670ff85e11ce09367db78661696a6fb1af
mike <mikeoconnor0308@gmail.com>
2018-09-24 15:29:19 +0100

    initial commit of simbox side

---
+++ simbox_qml/Simbox.QML/Load/QMLForceFieldTerms.cs
@@ -0,0 +1,31 @@
+﻿using Nano.Loading;
+using Nano.Science.Simulation.ForceField;
+using Nano.Science.Simulation.Instantiated;
+using Nano.Science.Simulation.Residues;
+using Nano.Science.Simulation.Spawning;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Simbox.QML.Load
+{
+    class QMLForceFieldTerms : IForceFieldTerms
+    {
+        public void AddToTopology(InstantiatedTopology parentTopology, IResidue residue, ref SpawnContext context)
+        {
+        }
+
+        public void Load(LoadContext context, XmlNode node)
+        {
+            //TODO add representations & configurations. 
+        }
+
+        public XmlElement Save(LoadContext context, XmlElement element)
+        {
+            return element;
+        }
+    }
+}
---
+++ simbox_qml/Simbox.QML/QMLForceField.cs
@@ -0,0 +1,26 @@
+﻿using Nano.Science.Simulation;
+using Nano.Science.Simulation.ForceField;
+using SlimMath;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simbox.QML
+{
+    public class QMLForceField : IForceField
+    {
+        public string Name => "QML Force Field";
+
+        public float CalculateForceField(IAtomicSystem system, List<Vector3> forces)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
