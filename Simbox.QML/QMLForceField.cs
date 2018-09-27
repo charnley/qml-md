7b24b42a6f340cdaab70d5fa54344b9efaffd157
mike <mikeoconnor0308@gmail.com>
2018-09-27 12:24:11 +0100

    working with a hardcoded conda environment path.

--- Simbox.QML/QMLForceField.cs
+++ Simbox.QML/QMLForceField.cs
@@ -58,15 +58,40 @@ namespace Simbox.QML
             //Dispose of things here.
         }
 
+
+        private void ExtractCondaEnvironment(string envName)
+        {
+            
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            //TODO generalise to other OS. 
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "activate & set PATH";
+            process.StartInfo = startInfo;
+            process.Start();
+            process.WaitForExit();
+            string output = process.StandardOutput.ReadToEnd();
+            
+           
+        }
+
         private void LoadPyConfiguration()
         {
+
             if (PythonHome != string.Empty)
             {
                 Reporter?.PrintDetail($"Python Home: {PythonHome} ");
                 //set up the python variables.
-                Environment.SetEnvironmentVariable("PATH", $@"{PythonHome};" + Environment.GetEnvironmentVariable("PATH"));
+                var pyPath = $@"{PythonHome};" + Environment.GetEnvironmentVariable("PATH");
+                //TODO these are all the paths that conda activate sets. We should extract these with a script deal with this properly! 
+                var condaPath = @"C:\Users\simbox-developer\AppData\Local\conda\conda\envs\net;C:\Users\simbox-developer\AppData\Local\conda\conda\envs\net\Library\mingw-w64\bin;C:\Users\simbox-developer\AppData\Local\conda\conda\envs\net\Library\usr\bin;C:\Users\simbox-developer\AppData\Local\conda\conda\envs\net\Library\bin;C:\Users\simbox-developer\AppData\Local\conda\conda\envs\net\Scripts;C:\Users\simbox-developer\AppData\Local\conda\conda\envs\net\bin;C:\ProgramData\Anaconda3;C:\ProgramData\Anaconda3\Library\mingw-w64\bin;C:\ProgramData\Anaconda3\Library\usr\bin;C:\ProgramData\Anaconda3\Library\bin;C:\ProgramData\Anaconda3\Scripts;C:\ProgramData\Anaconda3\bin";
+                condaPath = condaPath + ";" + Environment.GetEnvironmentVariable("PATH");
+
+                Environment.SetEnvironmentVariable("PATH", condaPath);
                 Environment.SetEnvironmentVariable("PYTHONHOME", PythonHome);
                 PythonEngine.PythonHome = PythonHome;
+                //PythonEngine.PythonPath = $@"{PythonHome};{PythonHome}\DLLs;{PythonHome}\Lib;{PythonHome}\Lib\site-packages;C:\Users\simbox-developer\AppData\Roaming\Python\Python35\site-packages'";
             }
             else
             {
@@ -79,12 +104,15 @@ namespace Simbox.QML
         private void ImportQML()
         {
             //specify the path to the qml_md python scripts.
-            var qmlDir = Helper.ResolvePath("~/Plugins/Simbox.QML/qml_md");
+            var qmlDir = Helper.ResolvePath("~/Plugins/Simbox.QML/");
             using(Py.GIL())
             {
                 dynamic sys = Py.Import("sys");
                 sys.path.append(qmlDir);
-                QmlPredict = Py.Import("predict");
+                dynamic qml = Py.Import("qml_md");
+
+                qml = Py.Import("qml.utils.alchemy");
+                QmlPredict = Py.Import("qml_md.predict");
             }
 
             
